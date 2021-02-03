b6d859824142dfa5620fceb10d2af70bbef07347
黎章龙 <lizhanglong@u-road.com>
2021-02-03 23:18:15 +0800

    项目搭建

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container">
+    <header class="qqpc">
+      <div class="title">
+        <ul>
+          <li class="logoLink">11</li>
+          <li class="item">首页</li>
+          <li class="item">下载</li>
+          <li class="item">动态</li>
+        </ul>
+      </div>
+    </header>
+    <section>
+      <router-view/>
+    </section>
+    <footer></footer>
+  </div>
+</template>
+
+<style lang="scss">
+body,html{
+  box-sizing: content-box;
+}
+.container{
+
+  width: 100%;
+  height: 100%;
+
+  header {
+    position: fixed;
+    z-index: 999;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 75px;
+    max-width: 1900px;
+    min-width: 960px;
+    margin: 0 auto;
+  }
+  .qqpc {
+    background: url(https://sqimg.qq.com/qq_product_operations/im/imbanner/20180201/pcqq_bg.jpg)
+      no-repeat center center;
+    background-size: cover;
+  }
+  .title {
+    width: 100%;
+    height: 75px;
+    background-color: transparent;
+    font-size: 0;
+
+    ul,li {
+      float: left;
+      list-style: none;
+    }
+
+    .item {
+      height: 75px;
+      line-height: 75px;
+      width: 75px;
+      text-align: center;
+      font-size: 20px;
+    }
+
+    .logoLink {
+      width: 106px;
+      height: 75px;
+      overflow: hidden;
+      font-size: 0;
+      background: transparent
+        url(//sqimg.qq.com/qq_product_operations/im/qqlogo/imlogo_b.png) no-repeat
+        left 15px;
+      margin-left: 300px;
+    }
+
+    .item:hover {
+      background-color: #12b7f5;
+    }
+  }
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="home">
+    <div class="content-item">内容1</div>
+    <div
+      class="activebg fisrtbg"
+      id="pic1"
+      v-bind:style="{
+        'background-position-x': positionX,
+        'background-position-y': positionY1 + 'px'
+      }"
+    ></div>
+    <div class="content-item">内容2</div>
+    <div
+      class="activebg secondbg"
+      id="pic2"
+      v-bind:style="{
+        'background-position-x': positionX,
+        'background-position-y': positionY2 + 'px'
+      }"
+    ></div>
+    <div class="content-item">内容3</div>
+    <div
+      class="activebg thirdbg"
+      id="pic3"
+      v-bind:style="{
+        'background-position-x': positionX,
+        'background-position-y': positionY3 + 'px'
+      }"
+    ></div>
+    <div class="content-item">内容4</div>
+    <div class="foot">foot</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      ratio: 0.05,
+      positionX: "50%",
+      positionY1: 30,
+      positionY2: 100,
+      positionY3: 150,
+      Y1: 0,
+      Y2: 0,
+      Y3: 0,
+    };
+  },
+  components: {},
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+    window.onload = () => {
+      let pic1 = document.getElementById("pic1");
+      let pic2 = document.getElementById("pic2");
+      let pic3 = document.getElementById("pic3");
+      this.positionY1 = this.Y1 = pic1.offsetTop * this.ratio;
+      this.positionY2 = this.Y2 = pic2.offsetTop * this.ratio;
+      this.positionY3 = this.Y3 = pic3.offsetTop * this.ratio;
+    };
+  },
+  methods: {
+    handleScroll() {
+      let scrollTop =
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop;
+      this.positionY1 = this.Y1 - scrollTop * this.ratio;
+      this.positionY2 = this.Y2 - scrollTop * this.ratio;
+      this.positionY3 = this.Y3 - scrollTop * this.ratio;
+
+    }
+  }
+};  
+</script>
+
+<style>
+
+.home {
+  height: 100%;
+}
+.content-item {
+  background-color: #fff;
+  width: 100%;
+  line-height: 400px;
+  text-align: center;
+  font-size: 30px;
+  font-weight: bold;
+}
+.activebg {
+  position: relative;
+  width: 100%;
+  height: 600px;
+  background: #fff;
+  background-attachment: fixed;
+  background-position: center 0;
+  background-repeat: no-repeat;
+}
+.fisrtbg {
+  background-image: url(https://sqimg.qq.com/qq_product_operations/im/2015/fisrtbg.jpg);
+}
+.secondbg {
+  background-image: url(https://sqimg.qq.com/qq_product_operations/im/2015/update/avd.jpg);
+}
+.thirdbg {
+  background-image: url(https://sqimg.qq.com/qq_product_operations/im/2015/blog.jpg);
+}
+.foot {
+  background-color: black;
+  line-height: 500px;
+  color: #fff;
+  text-align: center;
+  width: 100%;
+  font-size: 30px;
+  font-weight: bold;
+}
+</style>
